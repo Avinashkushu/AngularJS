3f2f6df785d18297f7bc9eb8e51acc55fcb3a064
Avinashkushu <147258797+Avinashkushu@users.noreply.github.com>
2025-04-24 21:48:36 +0530

    Update practice-a.component.ts

--- practice-a/practice-a.component.ts
+++ practice-a/practice-a.component.ts
@@ -1,15 +1,1 @@
-import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-practice-a',
-  templateUrl: './practice-a.component.html',
-  styleUrls: ['./practice-a.component.scss']
-})
-export class PracticeAComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
