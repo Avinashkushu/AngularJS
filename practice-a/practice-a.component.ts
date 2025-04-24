f6fa4d0608e3bd89c344efbd70e2ebd4f23c3e1e
Avinashkushu <147258797+Avinashkushu@users.noreply.github.com>
2025-04-24 21:40:00 +0530

    Add files via upload
    📝 User Registration Form (AngularJS)
    This is a simple web form built using AngularJS that allows users to submit their basic information. It demonstrates how to create a reactive form, validate user inputs, and handle form submissions in a clean and responsive UI.
    
    🚀 Features
    Built with AngularJS (1.x)
    
    Collects basic user information:
    
    First Name
    
    Last Name
    
    Email Address
    
    Phone Number
    
    City
    
    Real-time form validation
    
    Error handling with clear messages
    
    Easy to integrate into larger AngularJS applications
    
    Clean and minimal design.
    
    🛠️ Technologies Used
    AngularJS (1.x)
    
    HTML5 / CSS3
    
    ✍️ Author
    Avinash B V

---
+++ practice-a/practice-a.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-practice-a',
+  templateUrl: './practice-a.component.html',
+  styleUrls: ['./practice-a.component.scss']
+})
+export class PracticeAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
